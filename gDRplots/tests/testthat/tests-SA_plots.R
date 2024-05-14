ef8f5912c7313725a52dbf39b675685e2fbbcf5f
Janina Smola <janina.smola@contractors.roche.com>
2024-05-14 18:22:51 +0200

    tests: update SA plots

--- gDRplots/tests/testthat/tests-SA_plots.R
+++ gDRplots/tests/testthat/tests-SA_plots.R
@@ -3,10 +3,10 @@ context("Test SA_plots")
 test_that("prepareCurves works as expected", {
   mae <- gDRutils::get_synthetic_data("small")
   SE <- mae[[1]]
-  iR <-  rownames(SE)[1]
+  iR <- rownames(SE)[1]
   grouping <- "cId"
-  dt_metrics = gDRutils::convert_se_assay_to_dt(SE[iR], "Metrics")
-  dt_average = gDRutils::convert_se_assay_to_dt(SE[iR], "Averaged")
+  dt_metrics <- gDRutils::convert_se_assay_to_dt(SE[iR], "Metrics")
+  dt_average <- gDRutils::convert_se_assay_to_dt(SE[iR], "Averaged")
 
   plt <- grob_SA(dt_metrics = dt_metrics,
                  dt_average = dt_average,
