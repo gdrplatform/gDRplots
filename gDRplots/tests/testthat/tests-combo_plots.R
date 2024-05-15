39ec25c93af9b1593a23fa75f3f9847c658c620b
Janina Smola <janina.smola@contractors.roche.com>
2024-05-15 10:42:22 +0200

    tests: update combo plots

---
+++ gDRplots/tests/testthat/tests-combo_plots.R
@@ -0,0 +1,43 @@
+context("Test combo_plots")
+
+test_that("prepareCurves works as expected", {
+  cellline_name <- "cellline_FD"
+  drug1_name <- "drug_011"
+  drug2_name <- "drug_026"
+  norm_type <- "RV"
+  
+  mae <- gDRutils::get_synthetic_data("combo_matrix")
+  SE <- mae[["combination"]]
+  
+  plts_1 <- gDR_combo_plot(SE = SE,
+                           drug1_name = drug1_name,
+                           drug2_name = drug2_name, 
+                           cellline_name = cellline_name)
+  expect_is(plts_1, "list")
+  expect_equal(names(plts_1), c(names(gDRutils::get_combo_excess_field_names()), "iso_compare"))
+  expect_true(all(vapply(seq_along(plts_1), 
+                         function(i) grepl("GR", plts_1[[i]]$labels$title), logical(1))))
+
+  normalization_type <- "RV"
+  plts_2 <- gDR_combo_plot(SE = SE,
+                           drug1_name = drug1_name,
+                           drug2_name = drug2_name, 
+                           cellline_name = cellline_name,
+                           normalization_type = normalization_type)
+  expect_is(plts_2, "list")
+  expect_equal(names(plts_2), c(names(gDRutils::get_combo_excess_field_names()), "iso_compare"))
+  expect_true(all(vapply(seq_along(plts_2), 
+                         function(i) grepl(normalization_type, plts_2[[i]]$labels$title), logical(1))))
+  
+  
+  expect_error(gDR_combo_plot(SE = mae,
+                              drug1_name = "unknown_drug",
+                              drug2_name = drug2_name, 
+                              cellline_name = cellline_name),
+               "Assertion on 'SE' failed: Must inherit from class 'SummarizedExperiment'")
+  expect_error(gDR_combo_plot(SE = SE,
+                              drug1_name = "unknown_drug",
+                              drug2_name = drug2_name, 
+                              cellline_name = cellline_name),
+               "Assertion on 'drug1_name' failed: Must be element of set")
+})
