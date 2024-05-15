9957b1c00e75d4d987c347250c7570da83e16666
Janina Smola <janina.smola@contractors.roche.com>
2024-05-15 10:42:11 +0200

    tests: update SA plots

--- gDRplots/tests/testthat/tests-SA_plots.R
+++ gDRplots/tests/testthat/tests-SA_plots.R
@@ -7,7 +7,7 @@ test_that("prepareCurves works as expected", {
   grouping <- "cId"
   dt_metrics <- gDRutils::convert_se_assay_to_dt(SE[iR], "Metrics")
   dt_average <- gDRutils::convert_se_assay_to_dt(SE[iR], "Averaged")
-
+  
   plt <- grob_SA(dt_metrics = dt_metrics,
                  dt_average = dt_average,
                  grouping = grouping)
@@ -24,4 +24,13 @@ test_that("prepareCurves works as expected", {
   expect_is(plt, "gg")
   expect_true(grepl(normalization_type, plt[["labels"]][["y"]]))
   expect_length(plt[["layers"]], 3)
+  
+  expect_error(grob_SA(dt_metrics = as.list(dt_metrics),
+                       dt_average = dt_average,
+                       grouping = grouping),
+               "Check on 'dt_metrics' failed: Must be a data.table")
+  expect_error(grob_SA(dt_metrics = dt_metrics,
+                       dt_average = dt_average,
+                       grouping = "str"),
+               "Check on 'grouping' failed: Must be element of set")
 })
