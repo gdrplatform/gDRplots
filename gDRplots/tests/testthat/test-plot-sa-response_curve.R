e26bf93f1163b77451e9a152e544fab8797202fc
Janina Smola <janina.smola@contractors.roche.com>
2024-06-26 14:04:37 +0200

    chore: update plot title

--- gDRplots/tests/testthat/test-plot-sa-response_curve.R
+++ gDRplots/tests/testthat/test-plot-sa-response_curve.R
@@ -44,7 +44,9 @@ test_that("plot_dose_response_sa works as expected", {
 
 test_that("plot_dose_response_sa_by_CLs works as expected", {
   cellline_name <- gDRutils::get_env_identifiers("cellline_name")
+  clid <- gDRutils::get_env_identifiers("cellline")
   drug_name <- gDRutils::get_env_identifiers("drug_name")
+  gnumber <- gDRutils::get_env_identifiers("drug")
   
   mae <- gDRutils::get_synthetic_data("small")
   se <- mae[[1]]
@@ -55,19 +57,22 @@ test_that("plot_dose_response_sa_by_CLs works as expected", {
   drug_name_vec <- unique(dt_metrics[[drug_name]])[5:7]
   
   plts <- plot_dose_response_sa_by_CLs(dt_metrics = dt_metrics, 
-                         dt_average = dt_average)
+                                       dt_average = dt_average)
   expect_is(plts, "list")
-  expect_equal(names(plts), paste("GR", unique(dt_metrics[[drug_name]])))
+  expect_equal(names(plts), 
+               sprintf("%s (%s)", unique(dt_metrics[[drug_name]]), unique(dt_metrics[[gnumber]])))
   
   metric_growth <- "RV"
   plts <- plot_dose_response_sa_by_CLs(dt_metrics = dt_metrics, 
-                         dt_average = dt_average,
-                         cellline_name_vec = cellline_name_vec,
-                         drug_name_vec = drug_name_vec,
-                         metric_growth = metric_growth,
-                         colormap = c("#B9D3EE", "#FF6347", "#C2F970"))
+                                       dt_average = dt_average,
+                                       cellline_name_vec = cellline_name_vec,
+                                       drug_name_vec = drug_name_vec,
+                                       metric_growth = metric_growth,
+                                       colormap = c("#B9D3EE", "#FF6347", "#C2F970"))
   expect_is(plts, "list")
-  expect_equal(names(plts), paste("RV", drug_name_vec))
+  plotted_ <- intersect(drug_name_vec, unique(dt_metrics[[drug_name]]))
+  expect_equal(names(plts), 
+               sprintf("%s (%s)", plotted_, unique(dt_metrics[get(drug_name) %in% plotted_][[gnumber]])))
   expect_true(all(vapply(seq_along(plts), 
                          function(i) grepl(metric_growth, plts[[i]]$labels$y), logical(1))))
   
@@ -75,12 +80,13 @@ test_that("plot_dose_response_sa_by_CLs works as expected", {
   drug_name_vec_2 <- c(drug_name_vec, "drug_100")
   
   plts <- plot_dose_response_sa_by_CLs(dt_metrics = dt_metrics, 
-                         dt_average = dt_average,
-                         cellline_name_vec = cellline_name_vec_2,
-                         drug_name_vec = drug_name_vec_2)
+                                       dt_average = dt_average,
+                                       cellline_name_vec = cellline_name_vec_2,
+                                       drug_name_vec = drug_name_vec_2)
   expect_is(plts, "list")
+  plotted_ <- intersect(drug_name_vec_2, unique(dt_metrics[[drug_name]]))
   expect_equal(names(plts), 
-               paste("GR", intersect(drug_name_vec_2, unique(dt_metrics[[drug_name]]))))
+               sprintf("%s (%s)", plotted_, unique(dt_metrics[get(drug_name) %in% plotted_][[gnumber]])))
   plotted <- intersect(cellline_name_vec_2, unique(dt_metrics[[cellline_name]]))
   expect_true(all(vapply(seq_along(plts), 
                          function(i) all(plts[[i]]$plot_env$group_names == plotted), logical(1))))
