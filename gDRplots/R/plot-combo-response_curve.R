5b5848078b3d48d133d967cc451555b0178f6de7
Janina Smola <janina.smola@contractors.roche.com>
2024-08-12 14:24:23 +0200

    fix: issue with coloring dose-response curve for combo data

--- gDRplots/R/plot-combo-response_curve.R
+++ gDRplots/R/plot-combo-response_curve.R
@@ -153,7 +153,6 @@ plot_dose_response_combo <- function(dt_average,
 #' @inheritParams plot_dose_response_combo
 #' @param d_names character vector with drug names to be plotted (Drug Name);
 #'    if NULL - all available drugs will be plotted
-#' @param as_panel logical flag whether return list of plot or panel
 #'
 #' @return panel with plot with dose-response curves for selected cell line by drugs
 #'
@@ -166,8 +165,7 @@ plot_dose_response_combo <- function(dt_average,
 #' cl_name <- "cellline_IB"
 #' 
 #' plot_dose_response_combo_qc_panel(dt_average = dt_average,
-#'                                   cl_name = cl_name,
-#'                                   as_panel = FALSE)
+#'                                   cl_name = cl_name)
 #' 
 #' d_names <- c("drug_001", "drug_002")
 #' plot_dose_response_combo_qc_panel(dt_average = dt_average,
@@ -179,20 +177,22 @@ plot_dose_response_combo_qc_panel <- function(dt_average,
                                               cl_name,
                                               d_names = NULL,
                                               normalization_type = "GR",
-                                              as_panel = TRUE) {
+                                              colormap = NULL) {
+  
   
   cellline_name <- gDRutils::get_env_identifiers("cellline_name")
   clid <- gDRutils::get_env_identifiers("cellline")
   drug_name <- gDRutils::get_env_identifiers("drug_name")
   drug_name_2 <- gDRutils::get_env_identifiers("drug_name2")
   conc <- gDRutils::get_env_identifiers("concentration")
+  conc_2 <- gDRutils::get_env_identifiers("concentration2")
   
   checkmate::expect_data_table(dt_average)
   checkmate::assert_string(cl_name)
   checkmate::assert_choice(cl_name, choices = unique(dt_average[[cellline_name]]))
   checkmate::expect_character(d_names, null.ok = TRUE)
   checkmate::expect_choice(normalization_type, choices = c("GR", "RV"))
-  checkmate::expect_flag(as_panel)
+  checkmate::expect_character(colormap, null.ok = TRUE)
   
   available_drugs <- unique(dt_average[[drug_name]])
   if (is.null(d_names) || all(!d_names %in% available_drugs)) {
@@ -202,44 +202,76 @@ plot_dose_response_combo_qc_panel <- function(dt_average,
   }
   
   # check input data
-  drugs_combination <-
+  selecteted_combination <-
     unique(dt_average[get(cellline_name) == cl_name, .SD, .SDcols = c(cellline_name, drug_name, drug_name_2)])
+  selecteted_combination <- selecteted_combination[get(drug_name) %in% d_names, ]
   stopifnot("combination of drugs and cell line does not exist" =
-              any(d_names %in% drugs_combination[[drug_name]]))
+              any(d_names %in% selecteted_combination[[drug_name]]))
   
-  plt_tab <- drugs_combination[get(drug_name) %in% d_names, ]
-  plt_name <- sprintf("%s (%s x %s)",
-                      plt_tab[[cellline_name]], plt_tab[[drug_name]], plt_tab[[drug_name_2]]
-  )
-  data.table::setnames(plt_tab,
-                       old = c(cellline_name, drug_name, drug_name_2),
-                       new = c("cl_name", "drug1_name", "drug2_name"))
+  # panel title
+  cl_clid <- unique(dt_average[get(cellline_name) == cl_name, ][[clid]])
+  panel_title <- sprintf("%s (%s)", cl_name, cl_clid)
   
-  # list of plots for each drug
-  ls_plts <- purrr::pmap(plt_tab,
-                         plot_dose_response_combo,
-                         dt_average = dt_average,
-                         normalization_type = normalization_type)
-  names(ls_plts) <- plt_name
+  # filter data for normalization type
+  filter_expr <- substitute(normalization_type == norm_type, list(norm_type = normalization_type))
+  dt_avg <- dt_average[eval(filter_expr)]
   
-  final_plot <- if (as_panel) {
-    # plots update
-    ls_plts <- lapply(seq_along(ls_plts), function(i) {
-      ls_plts[[i]] <- ls_plts[[i]] +
-        ggplot2::ggtitle(label = plt_tab[["drug2_name"]][i]) +
-        ggplot2::labs(color = bquote(~.(conc) ~ "[" ~ mu * M ~ "]"))
-    })
-    # panel title
-    cl_clid <- unique(dt_average[get(cellline_name) == cl_name, ][[clid]])
-    panel_title <- sprintf("%s (%s)", cl_name, cl_clid)
-    
-    # final panel
-    ggpubr::annotate_figure(
-      ggpubr::ggarrange(plotlist = ls_plts, common.legend = TRUE, legend = "left"),
-      top = panel_title) +
-      ggpubr::bgcolor("white") + ggpubr::border("white")
-  } else {
-    ls_plts
+  # and selected cell line
+  required_cols <- c(cellline_name, drug_name, drug_name_2, conc, conc_2, "x")
+  dt_avg <- dt_avg[get(cellline_name) == cl_name, ][, .SD, .SDcols = required_cols]
+  
+  # filter data for combination cell line (drug x drug2)
+  dt_avg <- dt_avg[selecteted_combination, on = c(cellline_name, drug_name, drug_name_2)]
+  
+  dt_avg[[conc_2]] <- factor(dt_avg[[conc_2]])
+  
+  # handle conc = 0
+  min_conc <- min(dt_avg[dt_avg[[conc]] > 0, ][[conc]])
+  dt_avg[[conc]][dt_avg[[conc]] == 0] <- min_conc / 100
+
+  # colors
+  ls_conc_2 <- unique(dt_avg[[conc_2]])
+  if (is.null(colormap) || !all(vapply(colormap, is_valid_color, logical(1)))) {
+    number_of_color <- NROW(ls_conc_2)
+    colormap <-
+      rev(colorspace::sequential_hcl(number_of_color + 1, palette = "viridis")[seq_along(ls_conc_2)])
+  } else if (NROW(colormap) != NROW(ls_conc_2)) {
+    colormap <- grDevices::colorRampPalette(colormap)(NROW(ls_conc_2))
   }
-  return(final_plot)
+  names(colormap) <- levels(ls_conc_2)
+  
+  # set min and max values for y
+  ymin <- min(c(0, min(dt_avg$x)))
+  ymax <- max(c(1.2, max(dt_avg$x)))
+  
+  # final plot
+  plt <-
+    ggplot2::ggplot(dt_avg,
+                    ggplot2::aes(x = get(conc), y = x, color = get(conc_2), group = get(conc_2))) +
+    ggplot2::geom_hline(yintercept = 0, color = "#A9A9A9") +
+    ggplot2::geom_point() +
+    ggplot2::geom_line() +
+    ggplot2::scale_y_continuous(lim = c(ymin, ymax)) +
+    ggplot2::scale_x_continuous(trans = "log10") +
+    ggplot2::scale_color_manual(values = colormap, labels = sprintf("%.4f", as.numeric(levels(ls_conc_2)))) +
+    ggplot2::xlab(bquote(~ "Concentration of Drug [" ~ mu * M ~ "]")) +
+    ggplot2::ylab(sprintf("log10(%s)", normalization_type)) +
+    ggplot2::ggtitle(panel_title) +
+    ggplot2::labs(color = bquote(~ "Codrug [" ~ mu * M ~ "]")) +
+    ggplot2::theme_bw() +
+    ggplot2::theme(axis.text.x = ggplot2::element_text(size = 8, angle = 45, vjust = 1, hjust = 1, margin = ggplot2::margin()),
+                   axis.text.y = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
+                   plot.title = ggplot2::element_text(size = 10, margin = ggplot2::margin()),
+                   panel.grid.minor = ggplot2::element_blank(), 
+                   legend.text = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
+                   legend.title = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
+                   aspect.ratio = 1) +
+    ggplot2::facet_wrap(~get(drug_name) + get(drug_name_2)) +
+    ggplot2::theme(strip.background = ggplot2::element_blank(),
+                   strip.text = ggplot2::element_text(size = 8, face = "bold", hjust = 0, margin = ggplot2::margin()),
+                   legend.position = "left",
+                   plot.title = ggplot2::element_text(size = 12, hjust = 0.5, margin = ggplot2::margin()),
+                   panel.spacing = ggplot2::unit(0.5, "pt"))
+  
+  return(plt)
 }
