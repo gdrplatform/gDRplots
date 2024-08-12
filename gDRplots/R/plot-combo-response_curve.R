f7dae54021e7fd5c7a6bdd12cbef49caad3883e8
Janina Smola <janina.smola@contractors.roche.com>
2024-08-12 20:19:16 +0200

    fix: issue with coloring dose-response curve for combo data

--- gDRplots/R/plot-combo-response_curve.R
+++ gDRplots/R/plot-combo-response_curve.R
@@ -121,18 +121,20 @@ plot_dose_response_combo <- function(dt_average,
     ggplot2::geom_line() +
     ggplot2::scale_y_continuous(lim = c(ymin, ymax)) +
     ggplot2::scale_x_continuous(trans = "log10") +
-    ggplot2::scale_color_manual(values = colormap, labels = sprintf("%.4f", as.numeric(levels(ls_conc_2)))) +
+    ggplot2::scale_color_manual(values = colormap, 
+                                labels = sprintf("%.4f", as.numeric(levels(ls_conc_2)))) +
     ggplot2::xlab(bquote(.(drug1_name) ~ "[" ~ mu * M ~ "]")) +
     ggplot2::ylab(sprintf("log10(%s)", normalization_type)) +
     ggplot2::ggtitle(plt_title) +
     ggplot2::labs(color = bquote(.(drug2_name) ~ "[" ~ mu * M ~ "]")) +
     ggplot2::theme_bw() +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(size = 8, angle = 45, vjust = 1, hjust = 1),
-                   axis.text.y = ggplot2::element_text(size = 8),
-                   plot.title = ggplot2::element_text(size = 10),
-                   panel.grid.minor = ggplot2::element_blank(), 
-                   legend.position = "left",
-                   aspect.ratio = 1)
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(size = 8, angle = 45, vjust = 1, hjust = 1),
+      axis.text.y = ggplot2::element_text(size = 8),
+      plot.title = ggplot2::element_text(size = 10),
+      panel.grid.minor = ggplot2::element_blank(), 
+      legend.position = "left",
+      aspect.ratio = 1)
   
   # split
   if (split_by_conc) {
@@ -141,8 +143,9 @@ plot_dose_response_combo <- function(dt_average,
     
     plt <- plt + 
       ggplot2::facet_wrap(~get(conc_2), labeller = ggplot2::as_labeller(lbls)) + 
-      ggplot2::theme(legend.position = "none", 
-                     strip.text = ggplot2::element_text(face = "bold"))
+      ggplot2::theme(
+        legend.position = "none", 
+        strip.text = ggplot2::element_text(face = "bold"))
   }
   
   return(plt)
@@ -228,7 +231,7 @@ plot_dose_response_combo_qc_panel <- function(dt_average,
   # handle conc = 0
   min_conc <- min(dt_avg[dt_avg[[conc]] > 0, ][[conc]])
   dt_avg[[conc]][dt_avg[[conc]] == 0] <- min_conc / 100
-
+  
   # colors
   ls_conc_2 <- unique(dt_avg[[conc_2]])
   if (is.null(colormap) || !all(vapply(colormap, is_valid_color, logical(1)))) {
@@ -253,25 +256,28 @@ plot_dose_response_combo_qc_panel <- function(dt_average,
     ggplot2::geom_line() +
     ggplot2::scale_y_continuous(lim = c(ymin, ymax)) +
     ggplot2::scale_x_continuous(trans = "log10") +
-    ggplot2::scale_color_manual(values = colormap, labels = sprintf("%.4f", as.numeric(levels(ls_conc_2)))) +
+    ggplot2::scale_color_manual(values = colormap, 
+                                labels = sprintf("%.4f", as.numeric(levels(ls_conc_2)))) +
     ggplot2::xlab(bquote(~ "Concentration of Drug [" ~ mu * M ~ "]")) +
     ggplot2::ylab(sprintf("log10(%s)", normalization_type)) +
     ggplot2::ggtitle(panel_title) +
     ggplot2::labs(color = bquote(~ "Codrug [" ~ mu * M ~ "]")) +
     ggplot2::theme_bw() +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(size = 8, angle = 45, vjust = 1, hjust = 1, margin = ggplot2::margin()),
-                   axis.text.y = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
-                   plot.title = ggplot2::element_text(size = 10, margin = ggplot2::margin()),
-                   panel.grid.minor = ggplot2::element_blank(), 
-                   legend.text = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
-                   legend.title = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
-                   aspect.ratio = 1) +
+    ggplot2::theme(
+      axis.text.x = ggplot2::element_text(size = 8, angle = 45, vjust = 1, hjust = 1, margin = ggplot2::margin()),
+      axis.text.y = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
+      plot.title = ggplot2::element_text(size = 10, margin = ggplot2::margin()),
+      panel.grid.minor = ggplot2::element_blank(), 
+      legend.text = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
+      legend.title = ggplot2::element_text(size = 8, margin = ggplot2::margin()),
+      aspect.ratio = 1) +
     ggplot2::facet_wrap(~get(drug_name) + get(drug_name_2)) +
-    ggplot2::theme(strip.background = ggplot2::element_blank(),
-                   strip.text = ggplot2::element_text(size = 8, face = "bold", hjust = 0, margin = ggplot2::margin()),
-                   legend.position = "left",
-                   plot.title = ggplot2::element_text(size = 12, hjust = 0.5, margin = ggplot2::margin()),
-                   panel.spacing = ggplot2::unit(0.5, "pt"))
+    ggplot2::theme(
+      strip.background = ggplot2::element_blank(),
+      strip.text = ggplot2::element_text(size = 8, face = "bold", hjust = 0, margin = ggplot2::margin()),
+      legend.position = "left",
+      plot.title = ggplot2::element_text(size = 12, hjust = 0.5, margin = ggplot2::margin()),
+      panel.spacing = ggplot2::unit(0.5, "pt"))
   
   return(plt)
 }
