0e248f482ba3d8c7dd0facd98f6a2c6ee0314eef
Janina Smola <janina.smola@contractors.roche.com>
2024-10-23 17:35:24 +0200

    chore: unify plot_dose_response_sa_x

--- gDRplots/tests/testthat/test-plot-sa-response_curve.R
+++ gDRplots/tests/testthat/test-plot-sa-response_curve.R
@@ -4,65 +4,74 @@ test_that("plot_dose_response_sa works as expected", {
   mae <- gDRutils::get_synthetic_data("small")
   se <- mae[[1]]
   
-  grouping <- gDRutils::get_env_identifiers("cellline_name")
-  iR <- rownames(se)[1]
-  dt_metrics <- gDRutils::convert_se_assay_to_dt(se[iR], "Metrics")
-  dt_average <- gDRutils::convert_se_assay_to_dt(se[iR], "Averaged")
+  group_var <- gDRutils::get_env_identifiers("cellline_name")
+  selected_drug <- "drug_002"
+  dt_metrics <- gDRutils::convert_se_assay_to_dt(se, "Metrics")
+  dt_average <- gDRutils::convert_se_assay_to_dt(se, "Averaged")
   
   plt_1 <- plot_dose_response_sa(dt_metrics = dt_metrics,
                                  dt_average = dt_average,
-                                 grouping = grouping)
+                                 selection_name = selected_drug,
+                                 group_var = group_var)
   expect_is(plt_1, "gg")
   expect_equal(plt_1[["labels"]][["y"]], "GR")
   expect_length(plt_1[["layers"]], 3)
   
   plt_2 <- plot_dose_response_sa(dt_metrics = dt_metrics,
                                  dt_average = dt_average,
-                                 grouping = grouping,
-                                 colors_vec = rainbow(NROW(unique(dt_metrics[[grouping]]))))
+                                 group_var = group_var,
+                                 selection_name = selected_drug,
+                                 colors_vec = rainbow(NROW(unique(dt_metrics[[group_var]]))))
   expect_is(plt_2, "gg")
   expect_length(unique(ggplot2::ggplot_build(plt_2)$data[[2]][["colour"]]),
-                NROW(unique(dt_metrics[[grouping]])))
+                NROW(unique(dt_metrics[[group_var]])))
   
-  iC <- colnames(se)[1:2]
-  dt_metrics <- gDRutils::convert_se_assay_to_dt(se[iR, iC], "Metrics")
-  dt_average <- gDRutils::convert_se_assay_to_dt(se[iR, iC], "Averaged")
+  subset_celline <- c("cellline_BA", "cellline_CA")
   # lack of data for curve and small number of conc point
-  dt_metrics[, c("ec50", "x_inf", "x_0", "h", "r2")] <- NA
-  dt_average <- dt_average[Concentration %in% unique(dt_average$Concentration)[1:2], ]
+  dt_metrics_lack <- data.table::copy(dt_metrics)[CellLineName %in% subset_celline, ]
+  dt_metrics_lack[, c("ec50", "x_inf", "x_0", "h", "r2")] <- NA
+  dt_average_lack <- 
+    data.table::copy(dt_average)[Concentration %in% unique(dt_average$Concentration)[1:2], ]
   
-  plt_3 <- plot_dose_response_sa(dt_metrics = dt_metrics,
-                                 dt_average = dt_average,
-                                 grouping = grouping)
+  plt_3 <- plot_dose_response_sa(dt_metrics = dt_metrics_lack,
+                                 dt_average = dt_average_lack,
+                                 selection_name = selected_drug,
+                                 group_var = group_var)
   expect_is(plt_3, "gg")
   expect_equal(plt_3[["labels"]][["y"]], "GR")
   expect_length(plt_3[["layers"]], 3)
   expect_equal(names(plt_3[["guides"]][["guides"]]), "colour")
   
-  grouping <- gDRutils::get_env_identifiers("drug_name")
-  iC <- colnames(se)[1]
-  dt_metrics <- gDRutils::convert_se_assay_to_dt(se[, iC], "Metrics")
-  dt_average <- gDRutils::convert_se_assay_to_dt(se[, iC], "Averaged")
+  group_var <- gDRutils::get_env_identifiers("drug_name")
+  selected_celline <- "cellline_BA"
   normalization_type <- "RV"
   
   plt_4 <- plot_dose_response_sa(dt_metrics = dt_metrics,
                                  dt_average = dt_average,
-                                 grouping = grouping,
+                                 selection_name = selected_celline,
+                                 group_var = group_var,
                                  normalization_type = normalization_type,
                                  colors_vec = c("cadetblue", "orange", "darkblue"),
                                  plot_fit_flag = FALSE)
   expect_is(plt_4, "gg")
   expect_equal(plt_4[["labels"]][["y"]], normalization_type)
+  expect_true(grepl(selected_celline, plt_4[["labels"]][["title"]]))
   expect_length(plt_4[["layers"]], 2)
   
   expect_error(plot_dose_response_sa(dt_metrics = as.list(dt_metrics),
                                      dt_average = dt_average,
-                                     grouping = grouping),
+                                     selection_name = selected_celline,
+                                     group_var = group_var),
                "Assertion on 'dt_metrics' failed: Must be a data.table")
   expect_error(plot_dose_response_sa(dt_metrics = dt_metrics,
                                      dt_average = dt_average,
-                                     grouping = "str"),
-               "Assertion on 'grouping' failed: Must be element of set")
+                                     selection_name = c("cl_1", "cl_2")),
+               "Assertion on 'selection_name' failed: Must have length 1.")
+  expect_error(plot_dose_response_sa(dt_metrics = dt_metrics,
+                                     dt_average = dt_average,
+                                     selection_name = selected_celline,
+                                     group_var = "str"),
+               "Assertion on 'group_var' failed: Must be element of set")
 })
 
 test_that("plot_dose_response_sa_by_CLs works as expected", {
