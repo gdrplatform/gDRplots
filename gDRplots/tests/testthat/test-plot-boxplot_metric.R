e734511975e038cbc86f55dce0e9470d3a60995f
Janina Smola <janina.smola@contractors.roche.com>
2024-11-29 08:56:09 +0100

    tests: add boxplots for single-agent metrics

--- gDRplots/tests/testthat/test-plot-boxplot_metric.R
+++ gDRplots/tests/testthat/test-plot-boxplot_metric.R
@@ -1,12 +1,12 @@
 context("Test boxplot_metric")
 
-test_that("plot_boxplot_sa_metric_by_CLs works as expected", {
+test_that("plot_boxplot_metric_sa_by_CLs works as expected", {
   mae <- gDRutils::get_synthetic_data("combo_matrix")
   se <- mae[[gDRutils::get_supported_experiments("sa")]]
   
   dt_metrics <- gDRutils::convert_se_assay_to_dt(se, "Metrics")
   
-  plt_1 <- plot_boxplot_sa_metric_by_CLs(dt_metrics) # default
+  plt_1 <- plot_boxplot_metric_sa_by_CLs(dt_metrics) # default
   
   expect_is(plt_1, "gg")
   expect_true(grepl("GR", plt_1[["labels"]][["y"]]))
@@ -15,7 +15,7 @@ test_that("plot_boxplot_sa_metric_by_CLs works as expected", {
   expect_equal(sort(ggplot2::layer_scales(plt_1)$x$get_labels()),
                sort(unique(dt_metrics[["CellLineName"]])))
   
-  plt_2 <- plot_boxplot_sa_metric_by_CLs(dt_metrics,
+  plt_2 <- plot_boxplot_metric_sa_by_CLs(dt_metrics,
                                          normalization_type = "RV",
                                          metric = "x_AOC",
                                          colors_vec = "darkgreen")
@@ -25,8 +25,37 @@ test_that("plot_boxplot_sa_metric_by_CLs works as expected", {
   expect_length(plt_2[["layers"]], 3)
   expect_equal(unique(ggplot2::ggplot_build(plt_2)[["data"]][[2]][["fill"]]), "darkgreen")
   
-  plt_3 <- plot_boxplot_sa_metric_by_CLs(dt_metrics,
+  plt_3 <- plot_boxplot_metric_sa_by_CLs(dt_metrics,
                                          colors_vec = c("blue", "yellow"))
   expect_is(plt_3, "gg")
   expect_equal(unique(ggplot2::ggplot_build(plt_3)[["data"]][[2]][["fill"]]), "blue")
+  
+  plt_4 <- plot_boxplot_metric_sa_by_CLs(dt_metrics,
+                                         metric = "x_max",
+                                         grouped_flag = TRUE)
+  
+  ls_lbl_x <- unique(dt_metrics[, c("CellLineName", "Tissue"), with = FALSE])
+  data.table::setorderv(ls_lbl_x, "Tissue")
+  expect_is(plt_4, "gg")
+  expect_equal(ggplot2::layer_scales(plt_4)$x$get_labels(), ls_lbl_x[["CellLineName"]])
+  expect_length(unique(ggplot2::ggplot_build(plt_4)[["data"]][[2]][["fill"]]), 
+                NROW(unique(ls_lbl_x[["Tissue"]])))
+  
+  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = unlist(dt_metrics)),
+               "Assertion on 'dt_metrics' failed: Must be a data.table")
+  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
+                                             normalization_type = "XX"),
+               "Assertion on 'normalization_type' failed: Must be element of set")
+  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
+                                             metric = "xxx"),
+               "Assertion on 'metric' failed: Must be element of set")
+  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
+                                             fit_source = 1),
+               "Assertion on 'fit_source' failed: Must be of type 'string'")
+  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
+                                             grouped_flag = "yes"),
+               "Assertion on 'grouped_flag' failed: Must be of type 'logical flag'")
+  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
+                                             colors_vec = 1:3),
+               "Assertion on 'colors_vec' failed: Must be of type 'character'")
 })
