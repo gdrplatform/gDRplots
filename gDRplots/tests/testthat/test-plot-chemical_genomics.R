a730f0add8fb7b6556489f291d74f11ef236ea00
Bartosz Czech <bartosz.czech@contractors.roche.com>
2025-02-14 15:11:07 +0100

    Apply 1 suggestion(s) to 1 file(s)
    Co-authored-by: Janina Smola <janina.smola@contractors.roche.com>

--- gDRplots/tests/testthat/test-plot-chemical_genomics.R
+++ gDRplots/tests/testthat/test-plot-chemical_genomics.R
@@ -33,7 +33,7 @@ test_that("analyze_cgs works correctly", {
 test_that("plot_cgs_ranking works correctly", {
   
   results <- analyze_cgs(metrics_data, metrics = c("xc50", "x_max"), cell_line = "CellLineName_1")
-  plt <- plot_cgs_ranking(results, cell_line = "CellLineName_1", metric = "xc50")
+  plt <- plot_cgs_ranking(results, cl_name = "CellLineName_1", metric = "xc50")
   expect_is(plt, "ggplot")
   
   # rest with a different metric
