a138cf954ccb4cdfc7e64686ab41c27b4024db1f
Janina Smola <janina.smola@contractors.roche.com>
2025-03-11 12:48:26 +0100

    feat: add capping for combo metrics

--- gDRplots/R/plot-chemical_genomics.R
+++ gDRplots/R/plot-chemical_genomics.R
@@ -41,7 +41,7 @@ analyze_cgs <- function(dt_metrics,
   checkmate::assert_character(metrics, any.missing = FALSE)
   checkmate::assert_subset(metrics, choices = c("x_mean", "x_AOC_range", "xc50", "ec50", "x_max"), empty.ok = FALSE)
   checkmate::assert_true(all(metrics %in% names(dt_metrics)))
-  checkmate::assert_string(cl_name)
+  checkmate::assert_string(cl_name, null.ok = TRUE)
   checkmate::assert_subset(cl_name, choices = unique(dt_metrics[[cellline]]), empty.ok = TRUE)
   checkmate::assert_choice(normalization_type, choices = c("GR", "RV"))
   
@@ -50,7 +50,7 @@ analyze_cgs <- function(dt_metrics,
   
   # take care of Inf and NaN values in IC50 metrics
   if (any(is.infinite(dt_metrics$xc50))) {
-    warning("In `dt_metrics` some xc50 values are infinite.")
+    message("In `dt_metrics` some xc50 values are infinite.")
     dt_metrics <- dt_metrics[!is.infinite(get("xc50")), ]
   }
   
@@ -195,7 +195,7 @@ plot_cgs_ranking <- function(results,
     ggplot2::geom_col(color = gDRutils::get_settings_from_json("EDGE_COLOR",
                                                                system.file(package = "gDRplots", "settings.json"))) +
     ggplot2::labs(title = cl_name,
-                  y = paste0("\u0394 ", metric, " for ", norm_type),
+                  y = bquote(~ Delta ~ .(metric) ~ "for" ~ .(norm_type)),
                   x = "Ranked drugs",
                   caption = sprintf("Top results with FDR < %.2f are shown. If no results meet this threshold,
                                     the top %d results by p-value are displayed.",
