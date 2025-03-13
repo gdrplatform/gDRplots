833821ca8a050eb61111708a3f5cfcde8e51a5fb
Janina Smola <janina.smola@contractors.roche.com>
2025-03-13 12:42:10 +0100

    feat: add coloring points option to boxplots

--- gDRplots/R/plot-boxplot_metric.R
+++ gDRplots/R/plot-boxplot_metric.R
@@ -68,6 +68,9 @@ plot_boxplot_metric_sa <- function(
   drug_name <- gDRutils::get_env_identifiers("drug_name")
   drug_MOA <- gDRutils::get_env_identifiers("drug_moa")
   
+  checkmate::assert_data_table(dt_metrics)
+  checkmate::assert_choice(group_var, choices = c(cellline_name, drug_name))
+  
   # check input data
   if (group_var == cellline_name) {
     point_var <- drug_name
@@ -77,8 +80,6 @@ plot_boxplot_metric_sa <- function(
     col_var <- drug_MOA
   }
   
-  checkmate::assert_data_table(dt_metrics)
-  checkmate::assert_choice(group_var, choices = c(cellline_name, drug_name))
   checkmate::assert_choice(normalization_type, choices = c("GR", "RV"))
   numeric_columns <- names(dt_metrics)[vapply(dt_metrics, is.numeric, logical(1))]
   checkmate::assert_choice(metric, choices = numeric_columns)
@@ -384,9 +385,11 @@ plot_boxplot_metric_sa_by_drugs <- function(
 #' dt_scores <- gDRutils::convert_se_assay_to_dt(se = se,
 #'                                               assay_name = "scores")
 #' 
-#' plot_boxplot_metric_combo(dt_scores)
+#' plot_boxplot_metric_combo(dt_scores,
+#'                           group_var = "DrugeName)
 #' 
 #' plot_boxplot_metric_combo(dt_scores,
+#'                           group_var = "CellLineName",
 #'                           normalization_type = "RV",
 #'                           grouped_flag = TRUE)
 #' 
@@ -406,18 +409,21 @@ plot_boxplot_metric_combo <- function(
   drug_name <- gDRutils::get_env_identifiers("drug_name")
   drug_name_2 <- gDRutils::get_env_identifiers("drug_name2")
   
+  checkmate::assert_data_table(dt_scores)
+  checkmate::assert_choice(group_var, choices = c(cellline_name, drug_name))
+  
   # check input data
   if (group_var == cellline_name) {
     point_var <- "DrugCombination"
     col_var <- tissue
   } else if (group_var == drug_name) {
     point_var <- cellline_name
+    group_var <- "DrugCombination"
     message("Coloring box by group is not available for this scenario.")
     grouped_flag <- FALSE
     col_var <- NULL
   }
   
-  checkmate::assert_data_table(dt_scores)
   checkmate::assert_choice(normalization_type, choices = c("GR", "RV"))
   checkmate::assert_choice(metric, choices = c("hsa_score", "bliss_score"))
   checkmate::assert_names(names(dt_scores), 
@@ -647,6 +653,7 @@ plot_boxplot_metric_combo_by_drugs <- function(
     normalization_type = "GR",
     metric = "hsa_score",
     fit_source = "gDR",
+    colored_pts_flag = FALSE,
     colors_vec = NULL
 ) {
   
@@ -660,6 +667,7 @@ plot_boxplot_metric_combo_by_drugs <- function(
   checkmate::assert_names(names(dt_scores), 
                           must.include = c(cellline_name, drug_name, drug_name_2, metric))
   checkmate::assert_string(fit_source, null.ok = TRUE)
+  checkmate::assert_flag(colored_pts_flag)
   checkmate::assert_character(colors_vec, null.ok = TRUE)
   boxplot_fill <- 
     gDRutils::get_settings_from_json("BOXPLOT_FILL",
@@ -674,40 +682,17 @@ plot_boxplot_metric_combo_by_drugs <- function(
     gDRutils::get_settings_from_json("EDGE_COLOR",
                                      system.file(package = "gDRplots", "settings.json"))
   
-  # filter data for normalization type
-  filter_expr <- substitute(normalization_type == norm_type & fit_source == fit_src,
-                            list(norm_type = normalization_type, fit_src = fit_source))
-  dt_sco_norm <- dt_scores[eval(filter_expr)]
-  
-  dt_sco <- dt_sco_norm[, c(cellline_name, drug_name, drug_name_2, metric), with = FALSE]
-  dt_sco$DrugCombination <-
-    paste(dt_sco[[drug_name]], "x", dt_sco[[drug_name_2]])
-  
-  plt_title <- sprintf("Number of unique celllines: %s", NROW(unique(dt_sco[[cellline_name]])))
-  
-  fill_color <- if (is.null(colors_vec) || !all(vapply(colors_vec, is_valid_color, logical(1)))) {
-    boxplot_fill
-  } else {
-    colors_vec[1]
-  }
-  
   plt <- 
-    ggplot2::ggplot(data = dt_sco,
-                    mapping = ggplot2::aes(x = DrugCombination, y = get(metric))) +
-    ggplot2::geom_hline(yintercept = 0, color = hline_color, linetype = "solid") +
-    ggplot2::geom_boxplot(fill = fill_color, 
-                          color = edge_color, alpha = 0.25, staplewidth = 0.5,
-                          outliers = FALSE) +
-    ggplot2::theme(legend.position = "none") +
-    ggplot2::geom_jitter(width = 0.2, height = 0, color = jitter_poinst_color) +
-    ggplot2::labs(title = plt_title,
-                  y = get_hm_title(metric, normalization_type), 
-                  x = "") +
-    ggplot2::theme_bw() +
-    ggplot2::theme(axis.text.x = ggplot2::element_text(size = 8, angle = 90, vjust = 1, hjust = 1),
-                   axis.text.y = ggplot2::element_text(size = 8),
-                   plot.title = ggplot2::element_text(size = 10),
-                   panel.grid.minor = ggplot2::element_blank())
+    plot_boxplot_metric_combo(
+      dt_scores = dt_scores,
+      group_var = drug_name,
+      normalization_type = normalization_type,
+      metric = metric,
+      fit_source = fit_source,
+      grouped_flag = FALSE,
+      colored_pts_flag = colored_pts_flag,
+      colors_vec = colors_vec
+    )
   
   return(plt)
 }
