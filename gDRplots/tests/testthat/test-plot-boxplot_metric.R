cdbfbcfed4a50fed30df1481de654aad4a92cab4
Janina Smola <janina.smola@contractors.roche.com>
2025-03-13 13:57:38 +0100

    tests: add coloring points option to boxplots

--- gDRplots/tests/testthat/test-plot-boxplot_metric.R
+++ gDRplots/tests/testthat/test-plot-boxplot_metric.R
@@ -174,32 +174,6 @@ test_that("plot_boxplot_metric_sa_by_CLs works as expected", {
   expect_length(plt_4[["layers"]], 3)
   expect_equal(NROW(unique(ggplot2::ggplot_build(plt_4)[["data"]][[3]][["colour"]])),
                NROW(unique(dt_metrics[["DrugName"]])))
-  
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = unlist(dt_metrics)),
-               "Assertion on 'dt_metrics' failed: Must be a data.table")
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
-                                             normalization_type = "XX"),
-               "Assertion on 'normalization_type' failed: Must be element of set")
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
-                                             metric = "xxx"),
-               "Assertion on 'metric' failed: Must be element of set")
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics[, -c("CellLineName")]),
-               "Assertion on 'names\\(dt_metrics\\)' failed: Names must include the elements")
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
-                                             fit_source = 1),
-               "Assertion on 'fit_source' failed: Must be of type 'string'")
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
-                                             grouped_flag = "yes"),
-               "Assertion on 'grouped_flag' failed: Must be of type 'logical flag'")
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
-                                             colored_pts_flag = "ADD"),
-               "Assertion on 'colored_pts_flag' failed: Must be of type 'logical flag'")
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
-                                             colors_vec = 1:3),
-               "Assertion on 'colors_vec' failed: Must be of type 'character'")
-  expect_error(plot_boxplot_metric_sa_by_CLs(dt_metrics = dt_metrics,
-                                             with_inf = "yes"),
-               "Assertion on 'with_inf' failed: Must be of type 'logical flag'")
 })
 
 test_that("plot_boxplot_metric_sa_by_drugs works as expected", {
@@ -255,32 +229,138 @@ test_that("plot_boxplot_metric_sa_by_drugs works as expected", {
                sort(log10(dt_metrics[normalization_type == "GR", ][["xc50"]])))
   expect_equal(sort(ggplot2::layer_scales(plt_4)$x$get_labels()),
                sort(unique(dt_metrics[["DrugName"]])))
+})
+
+test_that("plot_boxplot_metric_combo works as expected", {
+  mae <- gDRutils::get_synthetic_data("combo_matrix")
+  se <- mae[[gDRutils::get_supported_experiments("combo")]]
+  dt_scores <- gDRutils::convert_se_assay_to_dt(se = se,
+                                                assay_name = "scores")
+  ls_comb <- unique(paste(dt_scores[["DrugName"]], "x", dt_scores[["DrugName_2"]]))
   
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = unlist(dt_metrics)),
-               "Assertion on 'dt_metrics' failed: Must be a data.table")
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = dt_metrics,
-                                               normalization_type = "XX"),
+  plt_1 <- plot_boxplot_metric_combo(dt_scores, 
+                                     group_var = "CellLineName") # default
+  expect_is(plt_1, "gg")
+  expect_length(plt_1[["layers"]], 3)
+  expect_equal(plt_1[["labels"]][["y"]], get_hm_title("hsa_score", "GR"))
+  expect_true(grepl("drug", plt_1[["labels"]][["title"]]))
+  expect_equal(sort(ggplot2::layer_scales(plt_1)$x$get_labels()),
+               sort(unique(dt_scores[["CellLineName"]])))
+  expect_true(grepl(NROW(ls_comb), plt_1[["labels"]][["title"]]))
+  
+  plt_2 <- plot_boxplot_metric_combo(dt_scores, 
+                                     group_var = "DrugName",
+                                     normalization_type = "RV",
+                                     metric = "bliss_score",
+                                     colored_pts_flag = TRUE,
+                                     colors_vec = "darkgreen")
+  expect_is(plt_2, "gg")
+  expect_length(plt_2[["layers"]], 3)
+  expect_equal(plt_2[["labels"]][["y"]], get_hm_title("bliss_score", "RV"))
+  expect_true(grepl("celllines", plt_2[["labels"]][["title"]]))
+  expect_equal(unique(ggplot2::ggplot_build(plt_2)[["data"]][[2]][["fill"]]), "darkgreen")
+  expect_true(grepl(NROW(unique(dt_scores[["CellLineName"]])), plt_2[["labels"]][["title"]]))
+  expect_equal(sort(ggplot2::layer_scales(plt_2)$x$get_labels()),
+               sort(ls_comb))
+  expect_equal(NROW(unique(ggplot2::ggplot_build(plt_2)[["data"]][[3]][["colour"]])),
+               NROW(unique(dt_scores[["CellLineName"]])))
+  
+  plt_3 <- plot_boxplot_metric_combo(dt_scores,
+                                     group_var = "CellLineName",
+                                     metric = "bliss_score",
+                                     colors_vec = c("blue", "yellow"))
+  expect_is(plt_3, "gg")
+  expect_equal(unique(ggplot2::ggplot_build(plt_3)[["data"]][[2]][["fill"]]), "blue")
+  
+  expect_message(
+    plt_4 <- plot_boxplot_metric_combo(dt_scores, 
+                                       group_var = "CellLineName",
+                                       normalization_type = "RV",
+                                       grouped_flag = TRUE,
+                                       colored_pts_flag = TRUE),
+    "Please, choose only one coloring options"
+  )
+  expect_is(plt_4, "gg")
+  expect_length(plt_4[["layers"]], 4) # grouped_flag
+  expect_true(grepl("col_var", plt_4[["labels"]][["fill"]]))
+  expect_true(grepl("point_var", plt_4[["labels"]][["colour"]]))
+  
+  expect_message(
+    plt_5 <- plot_boxplot_metric_combo(dt_scores, 
+                                       group_var = "DrugName",
+                                       normalization_type = "RV",
+                                       grouped_flag = TRUE,
+                                       colored_pts_flag = TRUE),
+    "Coloring box by group is not available for this scenario."
+  )
+  expect_is(plt_5, "gg")
+  expect_length(plt_5[["layers"]], 3) # grouped_flag ignored
+  expect_null(plt_5[["labels"]][["fill"]])
+  expect_true(grepl("point_var", plt_5[["labels"]][["colour"]]))
+  expect_equal(sort(ggplot2::layer_scales(plt_5)$x$get_labels()),
+               sort(ls_comb))
+  
+  dt_scores_2 <- data.table::copy(dt_scores)
+  ls_col_x <- c("CellLineName", "clid", "DrugName", "Gnumber")
+  dt_scores_2[, (ls_col_x) := lapply(.SD, paste0, "X"), .SDcols = ls_col_x]
+  dt_scores_2 <- rbind(dt_scores, dt_scores_2)
+  
+  plt_6 <- plot_boxplot_metric_combo(dt_scores_2, 
+                                     group_var = "DrugName",
+                                     normalization_type = "RV",
+                                     colored_pts_flag = TRUE)
+  expect_is(plt_6, "gg")
+  expect_length(plt_6[["layers"]], 3)
+  expect_equal(sort(ggplot2::ggplot_build(plt_6)[["data"]][[3]][["y"]]),
+               sort(dt_scores_2[normalization_type == "RV", ][["hsa_score"]]))
+  expect_equal(NROW(unique(ggplot2::ggplot_build(plt_6)[["data"]][[3]][["colour"]])), 
+               1) # too many points to be colored
+  
+  plt_7 <- plot_boxplot_metric_combo(dt_scores_2, 
+                                     group_var = "CellLineName",
+                                     metric = "bliss_score",
+                                     normalization_type = "RV",
+                                     colored_pts_flag = TRUE)
+  expect_is(plt_7, "gg")
+  expect_length(plt_7[["layers"]], 3)
+  expect_equal(sort(ggplot2::ggplot_build(plt_7)[["data"]][[3]][["y"]]),
+               sort(dt_scores_2[normalization_type == "RV", ][["bliss_score"]]))
+  expect_equal(NROW(unique(ggplot2::ggplot_build(plt_7)[["data"]][[3]][["colour"]])), 
+               1) # too many points to be colored
+  
+  expect_error(plot_boxplot_metric_combo(dt_scores = unlist(dt_scores),
+                                         group_var = "CellLineName"),
+               "Assertion on 'dt_scores' failed: Must be a data.table")
+  expect_error(plot_boxplot_metric_combo(dt_scores = dt_scores,
+                                         group_var = "unknown"),
+               "Assertion on 'group_var' failed: Must be element of set")
+  expect_error(plot_boxplot_metric_combo(dt_scores = dt_scores,
+                                         group_var = "CellLineName",
+                                         normalization_type = "XX"),
                "Assertion on 'normalization_type' failed: Must be element of set")
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = dt_metrics,
-                                               metric = "xxx"),
+  expect_error(plot_boxplot_metric_combo(dt_scores = dt_scores,
+                                         group_var = "CellLineName",
+                                         metric = "xxx"),
                "Assertion on 'metric' failed: Must be element of set")
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = dt_metrics[, -c("CellLineName")]),
-               "Assertion on 'names\\(dt_metrics\\)' failed: Names must include the elements")
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = dt_metrics,
-                                               fit_source = 1),
+  expect_error(plot_boxplot_metric_combo(dt_scores = dt_scores[, -c("CellLineName")],
+                                         group_var = "CellLineName"),
+               "Assertion on 'names\\(dt_scores\\)' failed: Names must include the elements")
+  expect_error(plot_boxplot_metric_combo(dt_scores = dt_scores,
+                                         group_var = "CellLineName",
+                                         fit_source = 1),
                "Assertion on 'fit_source' failed: Must be of type 'string'")
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = dt_metrics,
-                                               grouped_flag = "yes"),
+  expect_error(plot_boxplot_metric_combo(dt_scores = dt_scores,
+                                         group_var = "CellLineName",
+                                         grouped_flag = "yes"),
                "Assertion on 'grouped_flag' failed: Must be of type 'logical flag'")
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = dt_metrics,
-                                               colored_pts_flag = "ADD"),
+  expect_error(plot_boxplot_metric_combo(dt_scores = dt_scores,
+                                         group_var = "CellLineName",
+                                         colored_pts_flag = "TRUE"),
                "Assertion on 'colored_pts_flag' failed: Must be of type 'logical flag'")
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = dt_metrics,
-                                               colors_vec = 1:3),
+  expect_error(plot_boxplot_metric_combo(dt_scores = dt_scores,
+                                         group_var = "CellLineName",
+                                         colors_vec = 1:3),
                "Assertion on 'colors_vec' failed: Must be of type 'character'")
-  expect_error(plot_boxplot_metric_sa_by_drugs(dt_metrics = dt_metrics,
-                                               with_inf = "yes"),
-               "Assertion on 'with_inf' failed: Must be of type 'logical flag'")
 })
 
 test_that("plot_boxplot_metric_combo_by_CLs works as expected", {
@@ -321,26 +401,6 @@ test_that("plot_boxplot_metric_combo_by_CLs works as expected", {
   expect_length(unique(ggplot2::ggplot_build(plt_3)[["data"]][[2]][["fill"]]), 
                 NROW(unique(ls_lbl_x[["Tissue"]])))
   expect_true(grepl(NROW(ls_comb), plt_3[["labels"]][["title"]]))
-  
-  expect_error(plot_boxplot_metric_combo_by_CLs(dt_scores = unlist(dt_scores)),
-               "Assertion on 'dt_scores' failed: Must be a data.table")
-  expect_error(plot_boxplot_metric_combo_by_CLs(dt_scores = dt_scores,
-                                                normalization_type = "XX"),
-               "Assertion on 'normalization_type' failed: Must be element of set")
-  expect_error(plot_boxplot_metric_combo_by_CLs(dt_scores = dt_scores,
-                                                metric = "xxx"),
-               "Assertion on 'metric' failed: Must be element of set")
-  expect_error(plot_boxplot_metric_combo_by_CLs(dt_scores = dt_scores[, -c("CellLineName")]),
-               "Assertion on 'names\\(dt_scores\\)' failed: Names must include the elements")
-  expect_error(plot_boxplot_metric_combo_by_CLs(dt_scores = dt_scores,
-                                                fit_source = 1),
-               "Assertion on 'fit_source' failed: Must be of type 'string'")
-  expect_error(plot_boxplot_metric_combo_by_CLs(dt_scores = dt_scores,
-                                                grouped_flag = "yes"),
-               "Assertion on 'grouped_flag' failed: Must be of type 'logical flag'")
-  expect_error(plot_boxplot_metric_combo_by_CLs(dt_scores = dt_scores,
-                                                colors_vec = 1:3),
-               "Assertion on 'colors_vec' failed: Must be of type 'character'")
 })
 
 test_that("plot_boxplot_metric_combo_by_CLs works as expected", {
@@ -354,7 +414,7 @@ test_that("plot_boxplot_metric_combo_by_CLs works as expected", {
   
   expect_is(plt_1, "gg")
   expect_equal(plt_1[["labels"]][["y"]], get_hm_title("hsa_score", "GR"))
-  expect_true(grepl("cellline", plt_1[["labels"]][["title"]]))
+  expect_true(grepl("celllines", plt_1[["labels"]][["title"]]))
   expect_length(plt_1[["layers"]], 3)
   expect_equal(sort(ggplot2::layer_scales(plt_1)$x$get_labels()), sort(ls_comb))
   
