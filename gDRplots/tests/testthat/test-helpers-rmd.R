ae856b5f3725ae872670471b157777cdc12d547a
Janina Smola <janina.smola@contractors.roche.com>
2025-04-07 16:16:42 +0200

    tests: silence warning xfun::attr() is deprecated

--- gDRplots/tests/testthat/test-helpers-rmd.R
+++ gDRplots/tests/testthat/test-helpers-rmd.R
@@ -206,28 +206,42 @@ test_that("prep_nested_plot_chunk works as expected", {
       linklist[[drug]][[cl]][["RV"]] <- link_RV
     }
   }  
+  no_plots_in_section <-  NROW(unique(dt_metrics$CellLineName)) * NROW(c("GR", "RV")) * NROW(ls_color)
   
   res_1 <- prep_nested_plot_chunk(plt_list = plotlist, 
                                   chunk_name = "metric_col") # default
   expect_is(res_1, "list")
   expect_length(res_1, NROW(plotlist))
   expect_length(res_1, NROW(unique(dt_metrics$DrugName)))
-  expect_equal(sum(grepl("#####", res_1[[1]])), # the lowest lvl with plots - default
-               NROW(unique(dt_metrics$CellLineName)) * NROW(c("GR", "RV")) * NROW(ls_color))
+  expect_equal(sum(grepl("#####", res_1[[1]])), no_plots_in_section) # the lowest lvl with plots - default
   
+  chunk_name_2 <- "CHUNK"
   res_2 <- prep_nested_plot_chunk(plt_list = plotlist, 
-                                  chunk_name = "metric_col", 
+                                  chunk_name = chunk_name_2, 
                                   link_list = linklist,
                                   header_level = 1)
   expect_is(res_2, "list")
   expect_equal(sum(vapply(seq_along(res_2), function(i) grepl("# drug_", res_2[[i]][1]), logical(1))),
                NROW(res_2))
-  no_plots_in_section <-  NROW(unique(dt_metrics$CellLineName)) * NROW(c("GR", "RV")) * NROW(ls_color)
+  expect_equal(sum(grepl(chunk_name_2, res_2[[1]])), no_plots_in_section)
   expect_equal(sum(grepl("####", res_2[[1]])), no_plots_in_section)
   expect_true(all(vapply(seq_along(res_2), 
                          function(i) sum(grepl("_blank", res_2[[i]])) == no_plots_in_section, logical(1)))) # link_list
   
-  expect_error(prep_nested_plot_chunk(plt_list = dt_metrics, chunk_name = "metric_col"), 
+  linklist_noname <- linklist[1:5]
+  names(linklist_noname) <- NULL
+  res_3 <- prep_nested_plot_chunk(plt_list = plotlist[1:5], 
+                                  chunk_name = "metric_col", 
+                                  link_list = linklist_noname,
+                                  header_level = 2)
+  expect_is(res_3, "list")
+  expect_equal(sum(vapply(seq_along(res_3), function(i) grepl("## drug_", res_3[[i]][1]), logical(1))),
+               NROW(res_3))
+  expect_equal(sum(grepl("#####", res_3[[1]])), no_plots_in_section)
+  expect_false(all(vapply(seq_along(res_3), 
+                         function(i) sum(grepl("_blank", res_3[[i]])) == no_plots_in_section, logical(1)))) # link_list
+  
+    expect_error(prep_nested_plot_chunk(plt_list = dt_metrics, chunk_name = "metric_col"), 
                "Assertion on 'plt_list' failed: Must be of type 'list'")
   expect_error(prep_nested_plot_chunk(plt_list = plotlist, chunk_name = 123), 
                "Assertion on 'chunk_name' failed: Must be of type 'string'")
