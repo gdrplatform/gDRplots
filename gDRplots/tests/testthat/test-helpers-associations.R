57eea6fe5b186e936865efc3b238f34f5a984b0d
Janina Smola <janina.smola@contractors.roche.com>
2025-04-09 09:19:18 +0200

    tests: add function to calculate linear associations

---
+++ gDRplots/tests/testthat/test-helpers-associations.R
@@ -0,0 +1,10 @@
+context("Test associations")
+
+test_that("calc_assoc works as expected", {
+})
+
+test_that(".calc_assoc_vector works as expected", {
+})
+
+test_that(".calc_assoc_matrix works as expected", {
+})
