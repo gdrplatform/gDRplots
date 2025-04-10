dc0630213da3c361c4d9e651ea7fceaa4fce557d
Janina Smola <janina.smola@contractors.roche.com>
2025-04-10 08:48:31 -0700

    feat: add function to calculate linear associations

--- gDRplots/R/helpers-associations.R
+++ gDRplots/R/helpers-associations.R
@@ -18,9 +18,8 @@ calc_assoc <- function(X, Y) {
   checkmate::assert_matrix(X, mode = "numeric")
   checkmate::assert_names(rownames(X))
   checkmate::assert_multi_class(Y, c("matrix", "numeric"))
-  checkmate::assert_numeric(Y)
   if (is.matrix(Y)) checkmate::assert_names(rownames(Y))
-  if (is.vector(Y)) checkmate::assert_names(colnames(Y))
+  if (is.vector(Y)) checkmate::assert_names(names(Y))
 
   if (is.vector(Y)) {
     .calc_assoc_vector(X = X, Y = Y)
@@ -43,22 +42,27 @@ calc_assoc <- function(X, Y) {
 #' @keywords internal 
 .calc_assoc_vector <- function(X, Y) {
   
+  checkmate::assert_matrix(X, mode = "numeric")
+  checkmate::assert_names(rownames(X))
+  checkmate::assert_class(Y, "numeric")
+  checkmate::assert_names(names(Y))
+  
   if (stats::sd(Y, na.rm = TRUE) == 0) {
     warning("Y has no variance, rendering all associations void. Please double check this is correct.")
     na_dt <- data.table::data.table(
       feature = names(Y)
-      , est_beta = NA
-      , est_beta_se = NA
-      , posterior_mean = NA
-      , posterior_sd = NA
-      , prob_negative = NA
-      , prob_positive = NA
-      , rho = NA
-      , p_value = NA
-      , q_value = NA
-      , s_value = NA
-      , lfsr = NA
-      , lfdr = NA
+      , est_beta = NA_real_
+      , est_beta_se = NA_real_
+      , posterior_mean = NA_real_
+      , posterior_sd = NA_real_
+      , prob_negative = NA_real_
+      , prob_positive = NA_real_
+      , rho = NA_real_
+      , p_value = NA_real_
+      , q_value = NA_real_
+      , s_value = NA_real_
+      , lfsr = NA_real_
+      , lfdr = NA_real_
     )
     return(na_dt)
   }
@@ -107,6 +111,12 @@ calc_assoc <- function(X, Y) {
 #' 
 #' @keywords internal
 .calc_assoc_matrix <- function(X, Y) {
+  
+  checkmate::assert_matrix(X, mode = "numeric")
+  checkmate::assert_names(rownames(X))
+  checkmate::assert_matrix(Y, mode = "numeric")
+  checkmate::assert_names(rownames(Y))
+  
   # calculate if any columns have zero variance
   zero_var_cols <- which(apply(Y, 2, stats::sd, na.rm = TRUE) == 0)
   zero_var_cols_len <- length(zero_var_cols)
@@ -154,22 +164,22 @@ calc_assoc <- function(X, Y) {
   # use `cdsr_models` to calculate the linear model coefficients efficiently on large matrices
   res <- cdsrmodels::lin_associations(X = X, Y = Y)
   
+  # subset the data so that the correlations match the output of the table
+  available_genes <- rownames(stats::na.omit(res$rho))
+  
   # convert results from a `matrix` to a `data.table`
   res_dt <- data.table::as.data.table(res$res.table)
   
   # if Y only has 1 column (either originally, or because others were filtered out)
   # ensure that the `dep.var` column is listed, otherwise the renaming and 
   # re-ordering step below will fail
-  if (dim(Y)[2] == 1) {
-    res_dt[, dep.var := colnames(Y)[1]]
+  if (!"dep.var" %in% names(res_dt)) {
+    res_dt[, dep.var := rep(colnames(Y), each = NROW(available_genes))]
   }
-  
-  # subset the data so that the correlations match the output of the table
-  available_genes <- rownames(res$res.table)
   
   # combine each column of the correlation matrix into a single giant vector and
   # store it as the appropriate column in `res_dt`
-  res_dt[, rho := sapply(res$rho[available_genes, ], as.vector)] # nolint
+  res_dt[, rho := vapply(res$rho[available_genes, ], as.vector, numeric(1))]
   
   # re-order columns for a more human-friendly output
   res_dt <- res_dt[
