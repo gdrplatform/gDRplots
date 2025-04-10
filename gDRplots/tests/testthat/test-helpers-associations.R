47b7ce68f068b1d0de568626e65de2753977394f
Janina Smola <janina.smola@contractors.roche.com>
2025-04-10 09:24:18 -0700

    tests: add function to calculate linear associations

--- gDRplots/tests/testthat/test-helpers-associations.R
+++ gDRplots/tests/testthat/test-helpers-associations.R
@@ -73,7 +73,7 @@ test_that("calc_assoc works as expected", {
   expect_is(res_3, "data.table")
   expect_true(all(c(res_col_names, "response") %in% names(res_3)))
   expect_equal(NROW(res_3), NROW(colnames(X)[colSums(X[rownames(Y_all), ]) > 0]) * NCOL(Y_all))
-
+  
   expect_error(calc_assoc(X = matrix(LETTERS[1:9], nrow = 3), Y),
                "Assertion on 'X' failed: Must store numerics")
   expect_error(calc_assoc(X = matrix(1:9, nrow = 3), Y),
@@ -85,7 +85,6 @@ test_that("calc_assoc works as expected", {
 })
 
 test_that(".calc_assoc_vector works as expected", {
-  
   # scenario: vector Y has no variance 
   Y_vec_no_var <- rep(8, NROW(X))
   names(Y_vec_no_var) <- names(Y_vec)
@@ -99,7 +98,6 @@ test_that(".calc_assoc_vector works as expected", {
 })
 
 test_that(".calc_assoc_matrix works as expected", {
-  
   # scenario: matrix Y has no variance 
   Y_mat_no_var <- matrix(rep(0.88, NROW(X)), ncol = 1,
                          dimnames = list(rownames(Y), selected_metric))
@@ -110,4 +108,14 @@ test_that(".calc_assoc_matrix works as expected", {
   expect_true(all(c(res_col_names, "response") %in% names(res_5)))
   expect_true(all(res_5[, lapply(.SD, is.na), .SDcols = -c("feature", "response")]))
   expect_true(all(res_5[, lapply(.SD, is.numeric), .SDcols = -c("feature", "response")]))
+  
+  # scenario: one column in matrix Y has no variance 
+  Y_all_no_var <- Y_all
+  Y_all_no_var[, 3] <- 0.888
+  expect_warning({
+    res_6 <- calc_assoc(X, Y = Y_all_no_var)
+  }, "The following columns in Y have no variance")
+  expect_is(res_6, "data.table")
+  expect_true(all(c(res_col_names, "response") %in% names(res_6)))
+  expect_true(all(res_6[, lapply(.SD, is.numeric), .SDcols = -c("feature", "response")]))
 })
