c6008453fd46f04e0b6dd010880068146179420a
Janina Smola <janina.smola@contractors.roche.com>
2025-04-10 13:24:08 +0200

    chore: update tests

--- gDRplots/tests/testthat/test-helpers-rmd.R
+++ gDRplots/tests/testthat/test-helpers-rmd.R
@@ -347,7 +347,7 @@ test_that("save_plot throws error for non-existent directory", {
 test_that("save_plot throws error for non-write access. directory", {
   p <- ggplot2::ggplot(datasets::mtcars, ggplot2::aes(mpg, wt)) + ggplot2::geom_point()
   fixed_dir_path <- file.path(tempdir(), "plots")
-  dir.create(fixed_dir_path, mode = "0000")
+  dir.create(fixed_dir_path, mode = "0444")
   file_path <- file.path(fixed_dir_path, "test_plot")
   
   expect_error(save_plot(p, file_path, "svg"), "The specified directory does not have write access.")
