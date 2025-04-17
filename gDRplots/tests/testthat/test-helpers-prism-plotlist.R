9444793fa10d35695e760e64c9b58299501867d6
Janina Smola <janina.smola@contractors.roche.com>
2025-04-17 17:14:46 +0200

    tests: update create_PRISM_plot_list_x

--- gDRplots/tests/testthat/test-helpers-prism-plotlist.R
+++ gDRplots/tests/testthat/test-helpers-prism-plotlist.R
@@ -10,9 +10,9 @@ test_that("create_PRISM_plot_list_sa works as expected", {
   d_names <- c("drug_004", "drug_021")
   
   meta_data_path <- system.file("testdata/Model.csv", package = "gDRplots")
-  feature_sets <- c("CRISPRGeneEffect", "OmicsCNGene")
-  prefixes <- c("KO_", "CN_")
   metadata_columns <- "OncotreeLineage"
+  feat_data_path <- file.path(".", "depmap_data")
+  feature_sets <- c("CRISPRGeneEffect.csv", "OmicsCNGene.csv")
   
   # TODO in GDR-2710
   
@@ -21,71 +21,79 @@ test_that("create_PRISM_plot_list_sa works as expected", {
                                          dt_metrics = dt_metrics,
                                          dt_average = dt_average,
                                          meta_data_path = meta_data_path,
-                                         feature_sets = feature_sets,
-                                         prefixes = prefixes),
+                                         feat_data_path = feat_data_path,
+                                         feature_sets = feature_sets),
                "Assertion on 'drug_name_vec' failed: Must be of type 'character'")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,
                                          dt_metrics = unlist(dt_metrics),
                                          dt_average = dt_average,
                                          meta_data_path = meta_data_path,
-                                         feature_sets = feature_sets,
-                                         prefixes = prefixes),
+                                         feat_data_path = feat_data_path,
+                                         feature_sets = feature_sets),
                "Assertion on 'dt_metrics' failed: Must be a data.table")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,
                                          dt_metrics = dt_metrics,
                                          dt_average = unlist(dt_average),
                                          meta_data_path = meta_data_path,
-                                         feature_sets = feature_sets,
-                                         prefixes = prefixes),
+                                         feat_data_path = feat_data_path,
+                                         feature_sets = feature_sets),
                "Assertion on 'dt_average' failed: Must be a data.table")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,
                                          dt_metrics = dt_metrics,
                                          dt_average = dt_average,
                                          meta_data_path = 1:3,
-                                         feature_sets = feature_sets,
-                                         prefixes = prefixes),
+                                         feat_data_path = feat_data_path,
+                                         feature_sets = feature_sets),
                "Assertion on 'meta_data_path' failed: Must be of type 'string'")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,   
                                          dt_metrics = dt_metrics,
                                          dt_average = dt_average,
                                          meta_data_path = "path/meta_data.qs",
-                                         feature_sets = feature_sets,
-                                         prefixes = prefixes),
+                                         feat_data_path = feat_data_path,
+                                         feature_sets = feature_sets),
                "Assertion on 'File ext must be csv' failed: Must be TRUE")
+  expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,   
+                                         dt_metrics = dt_metrics,
+                                         dt_average = dt_average,
+                                         meta_data_path = meta_data_path,
+                                         feat_data_path = 1:5,
+                                         feature_sets = feature_sets),
+               "Assertion on 'feat_data_path' failed: Must be of type 'string'")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,
                                          dt_metrics = dt_metrics,
                                          dt_average = dt_average,
                                          meta_data_path = meta_data_path,
-                                         feature_sets = 1:2,
-                                         prefixes = prefixes),
+                                         feat_data_path = feat_data_path,
+                                         feature_sets = 1:2),
                "Assertion on 'feature_sets' failed: Must be of type 'character'")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,
                                          dt_metrics = dt_metrics,
                                          dt_average = dt_average,
                                          meta_data_path = meta_data_path,
-                                         feature_sets = feature_sets[1],
-                                         prefixes = prefixes),
-               "`prefixes` has to be the same length as `feature_sets`")
+                                         feat_data_path = feat_data_path,
+                                         feature_sets = feature_sets[1]),
+               "Assertion on 'feat_path' failed: File does not exist")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,
                                          dt_metrics = dt_metrics,
                                          dt_average = dt_average,
                                          meta_data_path = meta_data_path,
-                                         feature_sets = feature_sets,
-                                         prefixes = 1:2),
-               "Assertion on 'prefixes' failed: Must be of type 'character'")
+                                         feat_data_path = feat_data_path,
+                                         feature_sets = feature_sets),
+               "Assertion on 'feat_path' failed: File does not exist")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,
                                          dt_metrics = dt_metrics,
                                          dt_average = dt_average,
                                          meta_data_path = meta_data_path,
+                                         feat_data_path = feat_data_path,
                                          feature_sets = NULL,
-                                         prefixes = NULL),
+                                         metadata_columns = NULL),
                "Provide `feature_sets` or `metadata_columns` for DepMam subset")
   expect_error(create_PRISM_plot_list_sa(drug_name_vec = d_names,
                                          dt_metrics = dt_metrics,
                                          dt_average = dt_average,
                                          meta_data_path = meta_data_path,
+                                         feat_data_path = feat_data_path,
                                          feature_sets = NULL,
-                                         prefixes = NULL,
                                          metadata_columns = 1:3),
                "Assertion on 'metadata_columns' failed: Must be of type 'character'")
 })
@@ -101,9 +109,10 @@ test_that("create_PRISM_plot_list_combo works as expected", {
   d_names_2 <- c("drug_026", "drug_031")
   
   meta_data_path <- system.file("testdata/Model.csv", package = "gDRplots")
-  feature_sets <- c("CRISPRGeneEffect", "OmicsCNGene")
-  prefixes <- c("KO_", "CN_")
   metadata_columns <- "OncotreeLineage"
+  feat_data_path <- file.path(".", "depmap_data")
+  feature_sets <- c("CRISPRGeneEffect.csv", "OmicsCNGene.csv")
+
   
   # TODO in GDR-2710
   
@@ -113,88 +122,80 @@ test_that("create_PRISM_plot_list_combo works as expected", {
                                             dt_metrics = dt_metrics,
                                             dt_scores = dt_scores,
                                             meta_data_path = meta_data_path,
-                                            feature_sets = feature_sets,
-                                            prefixes = prefixes),
+                                            feat_data_path = feat_data_path,
+                                            feature_sets = feature_sets),
                "Assertion on 'drug1_name_vec' failed: Must be of type 'character'")
   expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
                                             drug2_name_vec = c(NA, NA),
                                             dt_metrics = dt_metrics,
                                             dt_scores = dt_scores,
                                             meta_data_path = meta_data_path,
-                                            feature_sets = feature_sets,
-                                            prefixes = prefixes),
+                                            feat_data_path = feat_data_path,
+                                            feature_sets = feature_sets),
                "Assertion on 'drug2_name_vec' failed: Contains only missing values")
   expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
                                             drug2_name_vec = d_names_2,
                                             dt_metrics = data.table::data.table(),
                                             dt_scores = dt_scores,
                                             meta_data_path = meta_data_path,
-                                            feature_sets = feature_sets,
-                                            prefixes = prefixes),
+                                            feat_data_path = feat_data_path,
+                                            feature_sets = feature_sets),
                "Assertion on 'dt_metrics' failed: Must have at least 1 rows")
   expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
                                             drug2_name_vec = d_names_2,
                                             dt_metrics = dt_metrics,
                                             dt_scores = unlist(dt_scores),
                                             meta_data_path = meta_data_path,
-                                            feature_sets = feature_sets,
-                                            prefixes = prefixes),
+                                            feat_data_path = feat_data_path,
+                                            feature_sets = feature_sets),
                "Assertion on 'dt_scores' failed: Must be a data.table")
   expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
                                             drug2_name_vec = d_names_2,
                                             dt_metrics = dt_metrics,
                                             dt_scores = dt_scores,
                                             meta_data_path = 1:3,
-                                            feature_sets = feature_sets,
-                                            prefixes = prefixes),
+                                            feature_sets = feature_sets),
                "Assertion on 'meta_data_path' failed: Must be of type 'string'")
   expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
                                             drug2_name_vec = d_names_2,
                                             dt_metrics = dt_metrics,
                                             dt_scores = dt_scores,
                                             meta_data_path = "path/meta_data.qs",
-                                            feature_sets = feature_sets,
-                                            prefixes = prefixes),
+                                            feat_data_path = feat_data_path,
+                                            feature_sets = feature_sets),
                "Assertion on 'File ext must be csv' failed: Must be TRUE")
   expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
                                             drug2_name_vec = d_names_2,
                                             dt_metrics = dt_metrics,
                                             dt_scores = dt_scores,
                                             meta_data_path = meta_data_path,
-                                            feature_sets = 1:2,
-                                            prefixes = prefixes),
+                                            feat_data_path = 1:2,
+                                            feature_sets = feature_sets),
+               "Assertion on 'feat_data_path' failed: Must be of type 'string'")
+  expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
+                                            drug2_name_vec = d_names_2,
+                                            dt_metrics = dt_metrics,
+                                            dt_scores = dt_scores,
+                                            meta_data_path = meta_data_path,
+                                            feat_data_path = feat_data_path,
+                                            feature_sets = 1:2),
                "Assertion on 'feature_sets' failed: Must be of type 'character'")
   expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
                                             drug2_name_vec = d_names_2,
                                             dt_metrics = dt_metrics,
                                             dt_scores = dt_scores,
                                             meta_data_path = meta_data_path,
-                                            feature_sets = feature_sets[1],
-                                            prefixes = prefixes),
-               "`prefixes` has to be the same length as `feature_sets`")
-  expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
-                                            drug2_name_vec = d_names_2,
-                                            dt_metrics = dt_metrics,
-                                            dt_scores = dt_scores,
-                                            meta_data_path = meta_data_path,
-                                            feature_sets = feature_sets,
-                                            prefixes = 1:2),
-               "Assertion on 'prefixes' failed: Must be of type 'character'")
-  expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
-                                            drug2_name_vec = d_names_2,
-                                            dt_metrics = dt_metrics,
-                                            dt_scores = dt_scores,
-                                            meta_data_path = meta_data_path,
+                                            feat_data_path = feat_data_path,
                                             feature_sets = NULL,
-                                            prefixes = NULL),
+                                            metadata_columns = NULL),
                "Provide `feature_sets` or `metadata_columns` for DepMam subset")
   expect_error(create_PRISM_plot_list_combo(drug1_name_vec = d_names,
                                             drug2_name_vec = d_names_2,
                                             dt_metrics = dt_metrics,
                                             dt_scores = dt_scores,
                                             meta_data_path = meta_data_path,
+                                            feat_data_path = feat_data_path,
                                             feature_sets = NULL,
-                                            prefixes = NULL,
                                             metadata_columns = 1:3),
                "Assertion on 'metadata_columns' failed: Must be of type 'character'")
 })
