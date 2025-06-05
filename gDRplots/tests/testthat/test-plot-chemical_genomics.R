ce36b3ba577b2061d2233fadee66cecc8f722c58
Bartek Czech <bartosz.w.czech@gmail.com>
2025-06-05 08:47:27 +0200

    test: fix unit tests

--- gDRplots/tests/testthat/test-plot-chemical_genomics.R
+++ gDRplots/tests/testthat/test-plot-chemical_genomics.R
@@ -42,7 +42,7 @@ test_that("analyze_cgs works correctly", {
                           cellline2 = "CellLineName_2",
                           normalization_type = "RV")
   expect_is(results4, "list")
-  expect_equal(names(results4), "CellLineName_1_CellLineName_2")
+  expect_equal(names(results4), "CellLineName_1 vs. CellLineName_2")
 })
 
 test_that("plot_cgs_ranking works correctly", {
