39cf08f27e47d736061ff5aedd63f57491ae0d20
Janina Smola <janina.smola@contractors.roche.com>
2025-12-03 08:39:22 +0000

    tests: update heatmap_combo_metrics_panel

--- gDRplots/tests/testthat/test-plot-combo-heatmaps.R
+++ gDRplots/tests/testthat/test-plot-combo-heatmaps.R
@@ -123,13 +123,11 @@ test_that("heatmap_combo_metrics_panel works as expected", {
   dt_excess <- gDRutils::convert_se_assay_to_dt(se, "excess")
   dt_isobolograms <- gDRutils::convert_se_assay_to_dt(se, "isobolograms")
   
-  expect_warning({ # remove when ggpubr will be updated to ggplot2 v.4.0.0. # nolint
-    plts_1 <- heatmap_combo_metrics_panel(dt_excess, 
-                                          dt_isobolograms,
-                                          drug1_name, 
-                                          drug2_name, 
-                                          cl_name) # default
-  })
+  plts_1 <- heatmap_combo_metrics_panel(dt_excess, 
+                                        dt_isobolograms,
+                                        drug1_name, 
+                                        drug2_name, 
+                                        cl_name) # default
   expect_is(plts_1, "gg")
   expect_length(ggplot2::ggplot_build(plts_1)$data, 1)
   
