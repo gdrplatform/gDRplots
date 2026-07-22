cac1b66f70352d627d132f757ba79aa69e62d9c3
Dariusz Scigocki <daro.scig@gmail.com>
2026-07-22 18:42:19 +0200

    chore: add package-specific BiocCheck note filters

---
+++ rplatform/valid_notes2.R
@@ -0,0 +1,32 @@
+global_valid_notes_l <- list(
+  list(
+    length = 1,
+    index_to_check = 1,
+    text_to_check = "Consider adding these automatically suggested biocViews"
+  ),
+  list(
+    length = 2,
+    index_to_check = 1,
+    text_to_check = "No 'fnd' role found in Authors@R"
+  ),
+  list(
+    length = 1,
+    index_to_check = 1,
+    text_to_check = "Consider adding runnable examples to man pages"
+  ),
+  list(
+    length = 1,
+    index_to_check = 1,
+    text_to_check = "Usage of dontrun"
+  ),
+  list(
+    length = 1,
+    index_to_check = 1,
+    text_to_check = "Use donttest"
+  ),
+  list(
+    length = 1,
+    index_to_check = 1,
+    text_to_check = "Consider shorter lines"
+  )
+)
